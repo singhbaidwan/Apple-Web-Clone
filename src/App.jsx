d770abe20ebbd4181f89d36f223eb30d27563fa4
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2024-04-16 00:08:40 +0530

    added nav bar layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+
 function App() {
-  return <main className="bg-black"></main>;
+  return (
+    <main className="bg-black">
+      <NavBar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
 }
 
 export default App;
